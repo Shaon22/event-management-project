8d09e944651965105d4de04560537b61b9698952
Shaon22 <shaonroy11@gmail.com>
2023-10-09 21:54:26 +0600

    updated firebase config

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -6,12 +6,12 @@ import {getAuth} from "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBSGN6K5x31NMOxjHxaYWKFEeRMphebsm4",
-  authDomain: "event-manage-project.firebaseapp.com",
-  projectId: "event-manage-project",
-  storageBucket: "event-manage-project.appspot.com",
-  messagingSenderId: "1083518608804",
-  appId: "1:1083518608804:web:c315d3fb57ca93a7e88d31"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
